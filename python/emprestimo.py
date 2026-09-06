f4b96a7637e7da1d182de37e02651878795f5972
Millene Rocha~ <millsrocha01@gmail.com>
2026-09-06 14:24:36 -0300

    Atualiza projeto

---
+++ python/emprestimo.py
@@ -0,0 +1,12 @@
+#Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
+casa = float(input('Valor da casa: R$'))
+salario = float(input('Salário do comprador: R$'))
+anos = int (input('Quantos anos de financiamento? '))
+prestacao = casa / (anos*12)
+minimo = salario * 30 / 100
+print('Para pagar uma casa no valor de R${:.2f} em {} anos, o valor da prestação será de R${:.2f}'.format(casa, anos, prestacao))
+if prestacao <= minimo:
+    print('Emprestimo pode ser concedido!')
+else:
+    print('Emprestimo negado!')
--- python/popo.py
+++
@@ -1,4 +0,0 @@
-echo funcioeqrfwerrqe xcxccvxvcxcsadsasaddsads
-
-
-jhjgh
